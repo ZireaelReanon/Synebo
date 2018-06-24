25fb2b0a3c1acd0571adbe70e4bccb6cc5ac9426
Alina <zireael.von.reanon@gmail.com>
2018-06-24 23:43:55 +0300

    Initial commit

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Solution { public int solution(int A, int B); }
+
+{
+    int count = 0;
+
+    
+    if (A < B)
+    {
+      
+        if (A < 0 && B < 0) return count;
+
+        
+        if (A <= 0) A = 1;
+        
+        int sqrtA = (int)Math.Sqrt(A);
+        int sqrtB = (int)Math.Sqrt(B);
+        count = sqrtB - sqrtA;
+       
+        if (A == sqrtA * sqrtA)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
---
+++ Task2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Solution { public int solution(int[] T); };
+        private int getSeason(DateTime date)
+        {
+            float value = (float)date.Month + date.Day / 100;   // <month>.<day(2 digit)>
+            if (value < 3.21 || value >= 12.22) return 3;   // Winter
+            if (value < 6.21) return 0; // Spring
+            if (value < 9.23) return 1; // Summer
+            return 2;   // Autumn
+        }
+        To include the seasons in the Southern Hemisphere the code can become:
+
+private int getSeason(DateTime date, bool ofSouthernHemisphere)
+        {
+            int hemisphereConst = (ofSouthernHemisphere ? 2 : 0);
+            Func<int, int> getReturn = (northern) => {
+                return (northern + hemisphereConst) % 4;
+            };
+            float value = (float)date.Month + date.Day / 100f;  // <month>.<day(2 digit)>
+            if (value < 3.21 || value >= 12.22) return getReturn(3);    // 3: Winter
+            if (value < 6.21) return getReturn(0);  // 0: Spring
+            if (value < 9.23) return getReturn(1);  // 1: Summer
+            return getReturn(2);    // 2: Autumn
+        }
+    }
+    }
+}
